ab1dea0536341d9fd8ad62a1c2450965d3875bab
Henrique Araujo <henriarasilva@gmail.com>
2017-09-28 03:16:51 -0300

    Adaptado as dependências antigas 2

--- src/HCRUDEasy.Web/wwwroot/dist/app.js
+++ src/HCRUDEasy.Web/wwwroot/dist/app.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "68f15fc68a390164d0d6"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "a84d0c5383f4b6d15ed5"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
